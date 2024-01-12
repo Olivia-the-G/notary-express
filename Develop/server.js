0a4dabe416efdc5f1085abce825b8042cc8a03a7
Flesh <olivia.a.gowan@gmail.com>
2024-01-12 13:36:23 -0500

    added html routes and get request for saved notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,35 @@
+// libraries 
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('./db/db.json');
 
 const app = express();
 
+// listening port 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+// middlewares 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+// confirmation for starting the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+// html routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// api routes 
+app.get('/api/notes', (req, res) => res.json(notes));
+
+// TODO: POST request for when a new note is created 
